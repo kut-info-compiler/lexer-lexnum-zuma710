a540cc05cdb1c1f5d1d084b141a1f83e8c05df47
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2018-10-10 22:09:11 +0900

    move the testing program to a separate repository

--- test.sh
+++ test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-while read instr outstr
-do
-    echo $instr | java Lexer > test.out
-    echo $instr `cat test.out` $outstr
-    echo $outstr | diff - test.out
-done < test.in
+TEST_DIR=../lexer-lexnum-00test
+export TEST_DIR
+
+TEST_SH=$TEST_DIR/test.sh
+TEST_REPO=https://github.com/kut-info-compiler/lexer-lexnum-00test.git
+
+if [ \! -x $TEST_SH ]; then
+    echo
+    echo "ERROR"
+    echo "Cannot find ${TEST_SH}."
+    echo "Clone the testing repository in the parent dir."
+    echo ""
+    echo "  > cd .."
+    echo "  > git clone ${TEST_REPO}"
+    echo ""
+    exit
+fi    
+
+exec $TEST_SH
